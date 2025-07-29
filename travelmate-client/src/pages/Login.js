8bcf6b9ad114713b96b876b1a43e9d4b419300c4
Anand Mishra <ayush19november@gmail.com>
2025-07-29 23:10:16 +0530

    Update Login.js

--- travelmate-client/src/pages/Login.js
+++ travelmate-client/src/pages/Login.js
@@ -9,7 +9,7 @@ const Login = () => {
 
   const handleLogin = async (e) => {
     e.preventDefault();
-    const res = await fetch("http://localhost:5000/api/auth/login", {
+    const res = await fetch(`${process.env.REACT_APP_API_BASE}/api/auth/login`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
