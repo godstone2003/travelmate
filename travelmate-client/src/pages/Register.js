38d27b4600b4f40f87b926f46a74e92b009fdf49
Anand Mishra <ayush19november@gmail.com>
2025-07-29 23:11:14 +0530

    Update Register.js

--- travelmate-client/src/pages/Register.js
+++ travelmate-client/src/pages/Register.js
@@ -9,7 +9,7 @@ const Register = () => {
 
   const handleRegister = async (e) => {
     e.preventDefault();
-    const res = await fetch("http://localhost:5000/api/auth/register", {
+    const res = await fetch(`${process.env.REACT_APP_API_BASE}/api/auth/register`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
